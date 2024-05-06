bb6399890566d84f2dbe46587e53f53f12645d45
MARCO <marcoanv13@gmail.com>
2024-05-06 00:14:17 -0500

    feat:commit initial config.

---
+++ src/main/java/com/example/msfacturacion/MsfacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.msfacturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsfacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsfacturacionApplication.class, args);
+	}
+
+}
